28e330f76d686f2f97ac05601dfe543d6a8b6f9d
TLNBS2405 <33620644+TLNBS2405@users.noreply.github.com>
2020-11-11 13:50:41 +0100

    Add kunimitsu.json

--- src/tools/new_character.py
+++ src/tools/new_character.py
@@ -1,46 +1,44 @@
-import csv
+import csv,os
 import json
-import tkfinder
-input_file = "Anna.csv"
-output_file = "../../json/anna.json"
+input_file = "Kuni.csv"
+character_name = "kunimitsu"
+json_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "..","..","json"))
+output_file = f'{json_folder}/{character_name}.json'
 
 movelist = []
 
 with open(input_file, encoding='utf-8') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    with open(output_file, encoding='utf-8') as original_json_file:
-        original_json_reader = json.load(original_json_file)
+    csv_reader = csv.reader(csv_file, delimiter=';')
 
-        for row in csv_reader:
-            new_move = {}
-            new_move['Command'] = row[0]
-            if row[1]:
-                aliases = []
-                positions= [pos for pos, char in enumerate(row[1]) if char == "\""]
+    for row in csv_reader:
+        new_move = {}
+        new_move['Command'] = row[1]
+        '''
+        if row[1]:
+            aliases = []
+            positions= [pos for pos, char in enumerate(row[1]) if char == "\""]
 
-                for x in range(0, len(positions), 2):
-                    alias = row[1][int(positions[x] +1): int(positions[x+1])]
-                    aliases.append(alias)
+            for x in range(0, len(positions), 2):
+                alias = row[1][int(positions[x] +1): int(positions[x+1])]
+                aliases.append(alias)
 
-                new_move['Alias'] = aliases
+            new_move['Alias'] = aliases'''
 
-            new_move['Hit level'] = row[2]
-            new_move['Damage'] = row[3]
-            new_move['Start up frame'] = row[4]
-            new_move['Block frame'] = row[5]
-            new_move['Hit frame'] = row[6]
-            new_move['Counter hit frame'] = row[7]
-            new_move['Notes'] = row[8]
-            new_move['Gif'] = ""
+        new_move['Hit level'] = row[2]
+        new_move['Damage'] = row[3]
+        new_move['Start up frame'] = row[4]
+        new_move['Block frame'] = row[5]
+        new_move['Hit frame'] = row[6]
+        new_move['Counter hit frame'] = row[7]
+        new_move['Notes'] = row[8]
+        new_move['Gif'] = row[0]
 
-            for move in original_json_reader:
-                if tkfinder.move_simplifier(move['Command']) == tkfinder.move_simplifier(row[0]):
-                    new_move['Gif'] = move['Gif']
-            for entry in new_move:
-                if type(new_move[entry]) != list and (new_move[entry] is None or not new_move[entry].strip()):
-                    new_move[entry] = "-"
+        for entry in new_move:
+            if type(new_move[entry]) != list and (new_move[entry] is None or not new_move[entry].strip()):
+                new_move[entry] = "-"
 
-            movelist.append(new_move)
+        movelist.append(new_move)
+    print(movelist)
 
 
 with open(output_file, 'w') as c:
